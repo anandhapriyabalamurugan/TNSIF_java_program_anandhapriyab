77117a702f40c0612f6af01e33fd4456e94c56f9
samsung <samsung@DESKTOP-757DLGP>
2025-06-29 10:44:40 +0530

    initialcommit

---
+++ TNSIF_JavaProject/src/com/tnsif/myprojects/InitialJavaCodes.java
@@ -0,0 +1,26 @@
+package com.tnsif.myprojects;
+
+public class InitialJavaCodes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 23;
+		double salary = 60000.00;
+		boolean ispassed = false;
+		char grade = 'B';
+			
+		double currentage = age;
+		int newsalary = (int) salary;
+		
+		int increment = 7000;
+		int total = increment + newsalary;
+		
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(newsalary);
+		System.out.println(total);
+		
+		
+	}
+
+}
